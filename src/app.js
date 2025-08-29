b7ae059b10fc76ba914adc2a745eab9e415a4819
Corey Rice <171101634+RiceC-at-MasonHS@users.noreply.github.com>
2025-08-28 21:19:44 -0400

    feat: show install prompt; use same-origin share target

--- src/app.js
+++ src/app.js
@@ -5,3 +5,32 @@ if ('serviceWorker' in navigator) {
         .then(() => console.log('Service Worker registered successfully.'))
         .catch((error) => console.error('Service Worker registration failed:', error));
 }
+
+// beforeinstallprompt handling to show an unobtrusive install button
+let deferredPrompt;
+const installButton = document.getElementById('installButton');
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    deferredPrompt = e;
+    // Show the install button
+    if (installButton) {
+        installButton.style.display = 'inline-block';
+        installButton.setAttribute('aria-hidden', 'false');
+        installButton.addEventListener('click', async () => {
+            installButton.style.display = 'none';
+            deferredPrompt.prompt();
+            const { outcome } = await deferredPrompt.userChoice;
+            console.log('User response to the install prompt:', outcome);
+            deferredPrompt = null;
+        });
+    }
+});
+
+window.addEventListener('appinstalled', (evt) => {
+    console.log('PWA was installed.');
+    if (installButton) {
+        installButton.style.display = 'none';
+    }
+});
